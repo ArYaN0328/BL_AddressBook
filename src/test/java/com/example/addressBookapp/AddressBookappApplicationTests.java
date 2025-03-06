c960988adffd093264f8eaf2ba4f0fa13f789a83
ArYaN0328 <121503458+ArYaN0328@users.noreply.github.com>
2025-03-06 22:33:18 +0530

    Boiler plate code

---
+++ src/test/java/com/example/addressBookapp/AddressBookappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.addressBookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
